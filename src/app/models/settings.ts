71853d86c7c15857e34dab36ac49729e54bdded1
Bulat R. Kutliev <ghostbulat@gmail.com>
2018-01-29 23:56:56 +0500

    Bucket renamed

--- src/app/models/settings.ts
+++ src/app/models/settings.ts
@@ -1,5 +1,5 @@
 export class Settings {
-	ApiEndPoint: string = "https://api.cosmicjs.com/v1/numerouno/";
+	ApiEndPoint: string = "https://api.cosmicjs.com/v1/parkingcare/";
 	ApiReadKey: string = "XpAQC8ytDL2Ae6xJLAfBXu7UOF5kIs1ontNdD07Upng1Mc4pwg";
 	ApiWriteKey: string = "LKhJLh7cEeoDpGJxwAWRaahbSOOXglnet0vcr61qsdCL4FvM24";
 }
