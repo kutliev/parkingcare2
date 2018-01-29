6494d789254d2aef8d34b9b9c9ced11328ad183a
Bulat R. Kutliev <ghostbulat@gmail.com>
2018-01-30 00:37:42 +0500

    Environment for prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  COSMIC_API: "https://api.cosmicjs.com/v1/",
+  COSMIC_BUCKET: "parkingcare",
+  COSMIC_READ_KEY: "XpAQC8ytDL2Ae6xJLAfBXu7UOF5kIs1ontNdD07Upng1Mc4pwg",
+  COSMIC_WRITE_KEY: "LKhJLh7cEeoDpGJxwAWRaahbSOOXglnet0vcr61qsdCL4FvM24",	
+  PORT: "4200"
 };
