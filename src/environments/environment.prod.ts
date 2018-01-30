ed48913780401d3828da3f6bcb7540be02fad8e8
Bulat R. Kutliev <ghostbulat@gmail.com>
2018-01-30 23:44:27 +0500

    Port updated to 5000, start line changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   COSMIC_BUCKET: "parkingcare",
   COSMIC_READ_KEY: "XpAQC8ytDL2Ae6xJLAfBXu7UOF5kIs1ontNdD07Upng1Mc4pwg",
   COSMIC_WRITE_KEY: "LKhJLh7cEeoDpGJxwAWRaahbSOOXglnet0vcr61qsdCL4FvM24",	
-  PORT: "5000"
+  PORT: "4200"
 };
